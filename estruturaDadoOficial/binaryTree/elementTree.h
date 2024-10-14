8eeecf6601649b353306f49a17ad96fa22341171
marcelocavl <marcelodev1604@gmail.com>
2024-10-14 15:03:11 -0300

    adicionei funçoes prototipos no arquivo elementTree.h

---
+++ estruturaDadoOficial/binaryTree/elementTree.h
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct elementTree
+{
+	void 			   *data;
+	struct elementTree *rightElement;
+	struct elementTree  *leftElement;
+}t_ElementTree;
+
+void 			        elementTree_create(void *data);
+void 		ElementTree_Delete(t_ElementTree *elementTree);
+
+void 	      elementTree_Get_Data(t_ElementTree *ElementTree);
+void  elementTree_Get_RightElement(t_ElementTree *ElementTree);
+void   elementTree_Get_LeftElement(t_ElementTree *ElementTree);
+
+void elementTree_Add_RightElement(t_ElementTree *elementTree,t_ElementTree elemetTreeToAdd);
+void elementTree_Add_LeftElementTree(t_ElementTree *elementTree,t_ElementTree elemetTreeToAdd);

fb5659a8c1e547a1548107ee8f343ad0432655e0
marcelocavl <marcelodev1604@gmail.com>
2024-10-15 00:28:35 -0300

    terminei de revisar e ajustar algumas funçoes

--- estruturaDadoOficial/binaryTree/mainBinaryTree.c
+++ estruturaDadoOficial/binaryTree/mainBinaryTree.c
@@ -15,6 +15,16 @@ int main()
 	int z=7;
 	element=elementTree_create(&x,sizeof(x));
 	dataDump(element->adressData,sizeof(element->sizeofData));
-	t_ElementTree *element2;    
+	element2=elementTree_create(&y,4);
+	element3=elementTree_create(&z,4);
+	//dataDump(element2->adressData,sizeof(element2->sizeofData));
+	//dataDump(element3->adressData,sizeof(element3->sizeofData));
+	
+	elementTree_Add_RightElement(element,element2);
+	elementTree_Add_LeftElementTree(element,element3);
+	dataDump(elementTree_Get_Data(element->rightElementTree),sizeof(element3->sizeofData));
+	dataDump(elementTree_Get_Data(element->leftElementTree),sizeof(element3->sizeofData));
+	
+	
 }
 
